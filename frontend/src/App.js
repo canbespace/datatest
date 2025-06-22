3bae7525c3fd11cfd321f8d3cf393811d3e57045
yourusernamecanbespace <canbespace@outlook.com>
2025-06-23 03:43:21 +0800

    Add login route to App.jsx

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Register from './pages/Register';
+import Login from './pages/Login';
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />   {/* 👈 Add this line */}
       </Routes>
     </BrowserRouter>
   );
