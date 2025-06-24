c0a6fb9637652729df803823126f4a7f9cd5e8bd
canbespace <mmzy2018@gmail.com>
2025-06-24 09:25:55 +0000

    protecting the registration page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,9 +4,6 @@ import Login from "./pages/Login";
 import AdminDashboard from "./pages/AdminDashboard"; // or "./pages/AdminDashboard"
 import AdminRoute from "./pages/AdminRoute";
 
-// removed the following line so the registration page is only accessible to the admin account
-//<Route path="/register" element={<Register />} />
-
 function App() {
   return (
     <BrowserRouter>
@@ -14,6 +11,15 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/knowledge" element={<Knowledge />} />
         <Route path="/dashboard" element={<Dashboard />} />
+
+        <Route
+          path="/register"
+          element={
+            <PrivateRoute>
+              <Register />
+            </PrivateRoute>
+          }
+        />
 
         <Route
           path="/admin"
--- frontend/src/pages/AdminRoute.js
+++ frontend/src/pages/AdminRoute.js
@@ -2,7 +2,6 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
 
-
 const AdminRoute = ({ children }) => {
   const token = localStorage.getItem("token");
   const role = localStorage.getItem("role");
