8ad914dba077580087efd0eca31321ebe873de36
canbespace <mmzy2018@gmail.com>
2025-07-01 07:13:21 +0000

    we good?

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import AdminDashboard from "./pages/admin/AdminDashboard";
-import AdminRoute from ".pages/AdminRoute";
+import AdminRoute from "./pages/AdminRoute";
 
 function App() {
   return (
