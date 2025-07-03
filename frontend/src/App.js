f65cc23b9be108acdab99b98e8b81daadaa7fe61
canbespace <mmzy2018@gmail.com>
2025-07-03 07:14:16 +0000

    070302

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,20 +3,17 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Knowledge from "./pages/Knowledge";
-import Unauthorized from "./pages/Unauthorized"; // ✅ add this ...
 import AdminDashboard from "./pages/admin/AdminDashboard";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/unauthorized" element={<Unauthorized />} />
+        <Route path="/" element={<Login />} />{" "}
         <Route path="/knowledge" element={<Knowledge />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/admin" element={<AdminDashboard />} />
-        <Route path="/admindashboard" element={<AdminDashboard />} />
       </Routes>
     </BrowserRouter>
   );
