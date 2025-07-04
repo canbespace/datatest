4a17204bf08437926f2d375d1598d403f11dce60
canbespace <mmzy2018@gmail.com>
2025-07-04 09:33:18 +0000

    might be the api link issue?

--- frontend/src/pages/Knowledge.jsx
+++ frontend/src/pages/Knowledge.jsx
@@ -13,7 +13,7 @@ const Knowledge = () => {
     const token = localStorage.getItem("token"); // or however you store it
 
     axios
-      .get("https://datatest-b2k5.onrender.com/api/knowledge", {
+      .get("https://datatest-b2k5.onrender.com/api/auth/knowledge", {
         headers: {
           Authorization: `Bearer ${token}`,
         },
